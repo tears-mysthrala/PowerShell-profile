653a29fb5df157b015956a1529dc5352d863b5f2
Tears Mysthrala <unaiup@gmail.com>
2025-09-16 20:10:34 +0200

    chore: remove obsolete PowerShell scripts and configuration files

--- run_check_empty_catch.ps1
+++
@@ -1,3 +0,0 @@
-Import-Module 'C:\Users\unaiu\OneDrive\Documents\PowerShell\Modules\PSScriptAnalyzer\1.24.0\PSScriptAnalyzer.psd1' -Force
-$res = Invoke-ScriptAnalyzer -Path 'Microsoft.PowerShell_profile.ps1' -IncludeRule 'PSAvoidUsingEmptyCatchBlock' -ErrorAction SilentlyContinue
-if ($null -eq $res -or $res.Count -eq 0) { Write-Output 'No findings for PSAvoidUsingEmptyCatchBlock in Microsoft.PowerShell_profile.ps1' } else { $res | Group-Object RuleName | ForEach-Object { "$($_.Name): $($_.Count)" } }
